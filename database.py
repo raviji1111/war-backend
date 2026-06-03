b32a49fd3e89eae4da0d861e86a6e6518b19e34c
Ravi Kumar <hack64448@gmail.com>
2026-06-03 10:15:47 +0530

    Switch to PostgreSQL database

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# This creates a local file named war_project.db to store our data
-SQLALCHEMY_DATABASE_URL = "sqlite:///./war_project.db"
+# Render se URL uthayega
+SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://war_db_user:I7g7HY7FFcORKIbC7aQfoaunN9vPTypl@dpg-d8fqti9kh4rs73ct4sj0-a/war_db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Agar local pe ho toh fallback
+if not SQLALCHEMY_DATABASE_URL:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./war_project.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
